e9d83352767edc7f6f36d4213622fed5a72045a0
Daniel Lamb <daniellamb113@gmail.com>
2019-09-21 14:41:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,6 @@ birthday_kids = {
   "Amanda" => 27
 }
 
-
 def happy_birthday(birthday_kids)
  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
