7f5cef8e592325201b925c73d69365df6eba6ba7
Daniel Lamb <daniellamb113@gmail.com>
2019-09-21 14:42:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,9 +1,11 @@
-
-
-
-
-
-
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 
 birthday_kids = {
   "Timmy" => 9,
